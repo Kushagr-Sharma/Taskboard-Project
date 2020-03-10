d20036a30aeed19a4f56abe074b2bb580a3d8fc6
Kushagr-Sharma <59696446+Kushagr-Sharma@users.noreply.github.com>
2020-03-10 17:37:26 +0530

    Second commit
    Made some vital changes with the responsive design. Also made a delete task button which gets rid of the task when it is complete. Made a more user friendly look to the App.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+var taskNumber=1;
 
 function addNew()
 {
@@ -9,13 +9,21 @@ function submit()
 {
   var entry=0;
   var x = document.getElementById('inputForm');
-  var mainContainer = document.getElementById('newEntry');
+  var mainContainer='<div id = "card'+taskNumber+'"><button class="delete" onclick="del(this)"></button>';
   for(var i = 0; i < x.length;i+=1)
   {
       entry++;
-      document.getElementById('newEntry').innerHTML+='<li class="entry'+entry+'">'+ x.elements[i].value + '</li>';
+      mainContainer+='<li class="entry'+entry+'">'+ x.elements[i].value + '</li>';
   }
-  document.getElementById('newEntry').innerHTML+='<br/><br/>'
+
+  mainContainer+='</div>';
+  document.getElementById('newEntry').innerHTML+=mainContainer;
   document.getElementById('inputForm').style.display="none";
   document.getElementById('submit').style.display="none";
+  document.getElementById('inputForm').reset();
+  taskNumber++;
+}
+function del(a)
+{
+  document.getElementById(a.parentNode.id).style.display="none";
 }
